8e2c8519bed239d9c1e8a53d3067e6a03f4a8937
vioghn <banafshehgholinejad802@gmail.com>
2022-05-18 21:05:12 +0430

    G12

--- openunderstand/main.py
+++ openunderstand/main.py
@@ -5,6 +5,11 @@ __author__ = "Navid Mousavizadeh, Amir Mohammad Sohrabi, Sara Younesi, Deniz Ahm
 __copyright__ = "Copyright 2022, The OpenUnderstand Project, Iran University of Science and technology"
 __credits__ = ["Dr.Parsa", "Dr.Zakeri", "Mehdi Razavi", "Navid Mousavizadeh", "Amir Mohammad Sohrabi", "Sara Younesi",
                "Deniz Ahmadi"]
+
+
+
+
+
 __license__ = "GPL"
 __version__ = "1.0.0"
 
